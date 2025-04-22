e11478e77a08345998a328a1e7806b687aaeeff1
HoonInPark <hooninpark@gmail.com>
2025-04-22 22:53:27 +0900

    del some unused module refs...

--- Source/HyperionMulti/HyperionMulti.Build.cs
+++ Source/HyperionMulti/HyperionMulti.Build.cs
@@ -8,6 +8,6 @@ public class HyperionMulti : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "HyperionBase" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine" });
 	}
 }
