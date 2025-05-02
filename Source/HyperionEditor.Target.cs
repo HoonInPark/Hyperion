67420415ddc11051af2ac75e7511c1d91dd72a86
HoonInPark <hooninpark@gmail.com>
2025-05-02 16:39:55 +0900

    making my dll in plugin dir

--- Source/HyperionEditor.Target.cs
+++ Source/HyperionEditor.Target.cs
@@ -12,6 +12,5 @@ public class HyperionEditorTarget : TargetRules
 		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
 		ExtraModuleNames.Add("Hyperion");
 		ExtraModuleNames.Add("HyperionBase");
-		ExtraModuleNames.Add("HyperionMulti");
 	}
 }
