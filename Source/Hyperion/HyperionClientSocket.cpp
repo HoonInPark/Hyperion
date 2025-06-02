4c69246b6cc50514effc2c78d22c8fcfc0c5c5d4
HoonInPark <hooninpark@gmail.com>
2025-06-02 16:06:02 +0900

    major error fixed

--- Source/Hyperion/HyperionClientSocket.cpp
+++ Source/Hyperion/HyperionClientSocket.cpp
@@ -46,15 +46,6 @@ void UHyperionClientSocket::OnReceive(const UINT32 _InSize)
 {
 	m_pPack->Read(m_RecvBuff, _InSize);
 
-	UE_LOG(LogTemp, Warning, TEXT("msg type: %d || cli idx %d || %f, %f, %f || %f, %f, %f"), 
-		static_cast<int>(m_pPack->GetMsgType()),
-		m_pPack->GetSessionIdx(),
-		m_pPack->GetPosX(),
-		m_pPack->GetPosY(),
-		m_pPack->GetPosZ(),
-		m_pPack->GetRotX(),
-		m_pPack->GetRotY(),
-		m_pPack->GetRotZ());
 	
 	switch (m_pPack->GetMsgType())
 	{
@@ -66,6 +57,15 @@ void UHyperionClientSocket::OnReceive(const UINT32 _InSize)
 	}
 	case MsgType::MSG_GAME:
 	{
+		UE_LOG(LogTemp, Warning, TEXT("msg type: %d || cli idx %d || %f, %f, %f || %f, %f, %f"), 
+			static_cast<int>(m_pPack->GetMsgType()),
+			m_pPack->GetSessionIdx(),
+			m_pPack->GetPosX(),
+			m_pPack->GetPosY(),
+			m_pPack->GetPosZ(),
+			m_pPack->GetRotX(),
+			m_pPack->GetRotY(),
+			m_pPack->GetRotZ());
 		auto pOwner = GetOwner();
 		if (auto pController = Cast<AHyperionPlayerController>(pOwner))
 		{
