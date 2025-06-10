a57bb39c3f06f44263c524a2dd39b38961a77bb6
HoonInPark <hooninpark@gmail.com>
2025-06-10 10:54:53 +0900

    anim class added

---
+++ Source/HyperionAnim/CharAnimInst.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "CharAnimInst.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HYPERIONANIM_API UCharAnimInst : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
